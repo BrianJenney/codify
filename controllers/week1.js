9ed95216e640e0c8275c8f2813b9eae8adfd764b
Brian Jenney <brianjenney83@gmail.com>
2016-09-24 21:46:54 -0700

    week 1 push to firebase with week object

--- controllers/week1.js
+++ controllers/week1.js
@@ -20,9 +20,32 @@ angular.module('myApp.week1', ['ngRoute','ui.bootstrap'])
 		$(this).parent().css({'display':'none'})
 	})
 
+	//initialize empty object for week 1
+	$scope.week1 = {};
+
 	$scope.exit = function(){
-		console.log("working")
 		$window.location.href = "/#/home"
+	}
+
+	$scope.submitWeek = function(){
+		console.log($scope.week1)
+		var user = firebase.auth().currentUser;
+
+		//if user hasn't marked the box, return false
+		function getValue(week){
+			if(week == 'undefined' || week == null){
+				return false;
+			}else{
+				return week;
+			}
+		}
+
+		//set firebase data with user's progress from checkboxes
+		firebase.database().ref('student/' + user.uid + '/week1/').set({
+			googleReplica: getValue($scope.week1.sendGoogleCode),
+			debugPico: getValue($scope.week1.sendDebugPico),
+			simpleReplica: getValue($scope.week1.sendSimpleReplica)
+		})
 	}
 
 
