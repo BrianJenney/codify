5c2802dca4f6ea05e142a4939ad0ab4d74ecf510
Brian Jenney <brianjenney83@gmail.com>
2016-09-30 21:32:40 -0700

    student table for admin page

--- controllers/admin.js
+++ controllers/admin.js
@@ -7,13 +7,52 @@ angular.module('myApp.admin', ['ngRoute'])
   });
 }])
 
-.controller('AdminCtrl', ['$scope',function($scope) {
+.controller('AdminCtrl', ['$scope','$filter',function($scope, $filter) {
 
+	//initialize array to store student object
+	$scope.studentArray = [];
+
+	//retrieve firebase data for students
 	firebase.database().ref('student/').once('value').then(function(snapshot){
 
-		console.log(snapshot.val())
-	})
+		//console.log(snapshot.val())
 
+		$scope.students = snapshot.val();
+
+		//get object from id key of student
+		angular.forEach($scope.students, function(value, key){
+		//console.log(value)
+		$scope.student = value;
+		$scope.$apply(function(){
+			$scope.studentArray.push(value)
+		})
+		
+
+		})
+		console.log($scope.studentArray)
+	})
  
- }]);
+ }])
+
+.filter("removeDups", function(){
+  return function(data) {
+    if(angular.isArray(data)) {
+      var result = [];
+      var key = {};
+      for(var i=0; i<data.length; i++) {
+        var val = data[i];
+        if(angular.isUndefined(key[val])) {
+          key[val] = val;
+          result.push(val);
+        }
+      }
+      if(result.length > 0) {
+        return result;
+      }
+    }
+    return data;
+  }
+})
+
+
 
